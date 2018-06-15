1a265bb727127635fccfbcb45a16155bdc38bc51
Aaron Drobek <adrobek09@gmail.com>
2018-06-15 07:54:44 -0400

    completed basic game functionality

--- src/main/java/com/drobek/practice/Controller/GameController.java
+++ src/main/java/com/drobek/practice/Controller/GameController.java
@@ -8,21 +8,169 @@ import java.util.List;
 
 public class GameController {
 
+    int diskToMove;
+    int diskToMoveIndex;
+    String towerOfDiscOrigin;
+    int discToValidate;
+    int discToValidateIndex;
 
-    public void buildGameBoard(int input, int[] towerA) {
-        for (int index = towerA.length; index > 0; index--) {
-            towerA[towerA.length - index] = index;
+
+    public void buildTowers(int[] tower) {
+        for (int index = tower.length; index > 0; index--) {
+            tower[tower.length - index] = index;
         }
     }
 
-    public void printGameBoard(int[] a,char x ) {
+
+    public void printTower(int[] a, char x) {
         System.out.print(x + "-");
-        for(int item: a){
+        for (int item : a) {
             System.out.print(item);
         }
         System.out.print("-");
         System.out.println();
     }
+
+    private void checkIfCanMoveFromTower(int[] tower, String towerName) {
+        for (int i = tower.length - 1; i >= 0; i--) {
+            if (tower[i] > 0) {
+                diskToMoveIndex = i;
+                diskToMove = tower[i];
+                towerOfDiscOrigin = towerName;
+                break;
+            }
+        }
+    }
+
+    public void towerOfHanoi(int numberOfDisks, char from, char to, int[] towerA, int[] towerB, int[] towerC) {
+        if (from == 'A') {
+            checkIfCanMoveFromTower(towerA, "towerA");
+        }
+        if (from == 'B') {
+            checkIfCanMoveFromTower(towerB, "towerB");
+        }
+        if (from == 'C') {
+            checkIfCanMoveFromTower(towerC, "towerC");
+        }
+        if (to == 'A') {
+            int emptyTower = 0;
+            for (int i = towerA.length - 1; i >= 0; i--) {
+                if (towerA[i] > 0) {
+                    discToValidate = towerA[i];
+                    discToValidateIndex = i;
+                    emptyTower++;
+                    if (validate()) {
+                        towerA[discToValidateIndex + 1] = diskToMove;
+                        if (from == 'B') {
+                            towerB[diskToMoveIndex] = 0;
+                            break;
+                        }
+                        if (from == 'C') {
+                            towerC[diskToMoveIndex] = 0;
+                            break;
+                        }
+                    }
+                }
+            }
+            if (emptyTower == 0) {
+
+                if (from == 'B') {
+                    moveMethod(towerA, towerB, diskToMove, diskToMoveIndex);
+                }
+                if (from == 'C') {
+                    moveMethod(towerA, towerC, diskToMove, diskToMoveIndex);
+                }
+            }
+        }
+        if (to == 'B') {
+            int emptyTower = 0;
+            for (int i = towerB.length - 1; i >= 0; i--) {
+                if (towerB[i] > 0) {
+                    discToValidate = towerB[i];
+                    discToValidateIndex = i;
+                    emptyTower++;
+                    if (validate()) {
+                        towerB[discToValidateIndex + 1] = diskToMove;
+                        if (from == 'A') {
+                            towerA[diskToMoveIndex] = 0;
+                            break;
+                        }
+
+                        if (from == 'C') {
+                            towerC[diskToMoveIndex] = 0;
+                            break;
+                        }
+                    }
+                }
+            }
+            if (emptyTower == 0) {
+
+                if (from == 'A') {
+                    moveMethod(towerB, towerA, diskToMove, diskToMoveIndex);
+                }
+                if (from == 'C') {
+                    moveMethod(towerB, towerC, diskToMove, diskToMoveIndex);
+                }
+            }
+        }
+        if (to == 'C') {
+            int emptyTower = 0;
+            for (int i = towerC.length - 1; i >= 0; i--) {
+                if (towerC[i] > 0) {
+                    discToValidate = towerC[i];
+                    discToValidateIndex = i;
+                    emptyTower++;
+                    if (validate()) {
+                        towerC[discToValidateIndex + 1] = diskToMove;
+                        if (from == 'B') {
+                            towerB[diskToMoveIndex] = 0;
+                            break;
+                        }
+
+                        if (from == 'A') {
+                            towerA[diskToMoveIndex] = 0;
+                            break;
+                        }
+                    }
+                }
+            }
+            if (emptyTower == 0) {
+                if (from == 'A') {
+                    moveMethod(towerC, towerA, diskToMove, diskToMoveIndex);
+                }
+
+                if (from == 'B') {
+                    moveMethod(towerC, towerB, diskToMove, diskToMoveIndex);
+                }
+            }
+        }
+
+    }
+
+    public void moveMethod(int[] xtower, int[] ytower, int diskToMove, int diskToMoveIndex) {
+        xtower[0] = diskToMove;
+        ytower[diskToMoveIndex] = 0;
+    }
+
+    public boolean validate() {
+        if (diskToMove < discToValidate) {
+            //can move
+            return true;
+        } else {
+            System.out.println("not valid move! you cannot place a larger number on top of a smaller number");
+            return false;
+        }
+
+    }
+
+    public boolean checkWin(int[] towerB, int[] towerC, int userInput) {
+        if (towerB[userInput - 1] > 0 || towerC[userInput - 1] > 0) {
+            System.out.println("********* YOU WON !*********");
+            return true;
+        }
+        return false;
+    }
+
 
 }
 
--- src/main/java/com/drobek/practice/Main.java
+++ src/main/java/com/drobek/practice/Main.java
@@ -5,29 +5,57 @@ import com.drobek.practice.Controller.GameController;
 import java.util.Scanner;
 
 public class Main {
-public static Scanner scanner = new Scanner(System.in);
-    public static void main(String... arg){
-        GameController gameController = new GameController();
-        System.out.println("How many discs would you like to play with?");
-        int userInput = scanner.nextInt();
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String... arg) {
+
+        int userInput;
+        do {
+            System.out.println("How many discs would you like to play with?");
+            while (!scanner.hasNextInt()) {
+                System.out.println("Number is required !");
+                scanner.next();
+            }
+            userInput = scanner.nextInt();
+        }
+        while (userInput <= 0);
+
         int[] towerA = new int[userInput];
         int[] towerB = new int[userInput];
         int[] towerC = new int[userInput];
+        int moveCounter = 0;
 
-        gameController.buildGameBoard(userInput, towerA);
-        gameController.printGameBoard(towerA, 'A');
-        gameController.printGameBoard(towerB, 'B');
-        gameController.printGameBoard(towerC, 'C');
+        GameController gameController = new GameController();
+        gameController.buildTowers(towerA);
 
+        while (gameController.checkWin(towerB, towerC, userInput) == false) {
 
-//        System.out.println("How many discs would you like? 1-5 ");
-//        int userInput = scanner.nextInt();
-//        gameController.validateUserEntry(userInput);
-////        gameController.buildGameBoard(userInput);
-//        gameController.printGameBoard();
+            gameController.printTower(towerA, 'A');
+            gameController.printTower(towerB, 'B');
+            gameController.printTower(towerC, 'C');
+            System.out.println();
 
+            System.out.println("Move--FROM");
+            char from = scanner.next().toUpperCase().charAt(0);
+            if (from == 'A' || from == 'B' || from == 'C') {
+                System.out.println("Move--TO");
+                char to = scanner.next().toUpperCase().charAt(0);
+                if (to != from) {
+                    if (to == 'A' || to == 'B' || to == 'C') {
+                        System.out.println();
 
-
-
+                        gameController.towerOfHanoi(userInput, from, to, towerA, towerB, towerC);
+                        moveCounter++;
+                    } else {
+                        System.out.println("please enter A B or C");
+                    }
+                } else {
+                    System.out.println("please enter A B or C");
+                }
+            }
+        }
+        System.out.println("Took you " + moveCounter + " moves to move the tower");
     }
+
 }
+
